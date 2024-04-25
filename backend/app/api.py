0dd17874da6bdc41d569d268b99114a1fd8f7d24
mugdhakolte <koltemugdha@gmail.com>
2024-04-25 12:58:46 +0200

    Added Delete API and frontend

--- backend/app/api.py
+++ backend/app/api.py
@@ -47,3 +47,12 @@ async def update_todo(id: int, body: dict) -> dict:
             todo["item"] = body["item"]
             return {"data": f"Todo with id {id} has been updated."}
     return {"data": f"Todo with id {id} not found."}
+
+
+@app.delete("/todo/{id}", tags=["todos"])
+async def delete_todo(id: int) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todos.remove(todo)
+            return {"data": f"Todo with id {id} has been removed"}
+    return {"data": f"Todo with id {id} not found"}
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -16,6 +16,7 @@ import { Box,
     useDisclosure
  } from "@chakra-ui/react";
 
+
 function TodoHelper({item, id, fetchTodos}) {
     return (
         <Box p={1} shadow="sm">
@@ -24,6 +25,7 @@ function TodoHelper({item, id, fetchTodos}) {
             {item}
             <Flex align="end">
                 <UpdateTodo item={item} id={id} fetchTodos={fetchTodos}/>
+                <DeleteTodo id={id} fetchTodos={fetchTodos}/>
             </Flex>
             </Text>
         </Flex>
@@ -142,3 +144,20 @@ function UpdateTodo({item, id}) {
         </>
     )
 }
+
+function DeleteTodo({id}) {
+    const {fetchTodos} = React.useContext(TodosContext)
+  
+    const deleteTodo = async () => {
+      await fetch(`http://localhost:8000/todo/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: { "id": id }
+      })
+      await fetchTodos()
+    }
+  
+    return (
+      <Button h="1.5rem" size="sm" onClick={deleteTodo}>Delete Todo</Button>
+    )
+  }
