81e4b7abf998ff88426047483ed68b12b054d33d
mugdhakolte <koltemugdha@gmail.com>
2024-04-25 12:50:17 +0200

    Added update todos js code

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,9 +1,35 @@
 import React, {useEffect, useState} from "react";
-import { Box, Button, Flex, 
-    Input, InputGroup, Modal, ModalBody,
-    ModalCloseButton, ModalContent, ModalFooter,
-    ModalHeader, ModalOverlay, Stack, Text, useDisclosure
+import { Box, 
+    Button, 
+    Flex, 
+    Input, 
+    InputGroup, 
+    Modal, 
+    ModalBody,
+    ModalCloseButton, 
+    ModalContent, 
+    ModalFooter,
+    ModalHeader, 
+    ModalOverlay, 
+    Stack, 
+    Text, 
+    useDisclosure
  } from "@chakra-ui/react";
+
+function TodoHelper({item, id, fetchTodos}) {
+    return (
+        <Box p={1} shadow="sm">
+        <Flex justify="space-between">
+            <Text mt={4} as="div">
+            {item}
+            <Flex align="end">
+                <UpdateTodo item={item} id={id} fetchTodos={fetchTodos}/>
+            </Flex>
+            </Text>
+        </Flex>
+        </Box>
+)
+}
 
 
 const TodosContext = React.createContext({
@@ -58,13 +84,61 @@ export default function Todos() {
         fetchTodos()
     }, [])
 
-    return (<TodosContext.Provider value={{todos, fetchTodos}}>
-        <AddTodo />
-        <Stack spacing={5}>
-            {todos.map((todo) => (
-                <b>{todo.item}</b>
-            )
-        )}
-        </Stack>
-    </TodosContext.Provider>)
+    return (
+        <TodosContext.Provider value={{todos, fetchTodos}}>
+          <AddTodo />
+          <Stack spacing={5}>
+            {
+              todos.map((todo) => (
+                <TodoHelper item={todo.item} id={todo.id} fetchTodos={fetchTodos} />
+              ))
+            }
+          </Stack>
+        </TodosContext.Provider>
+      )
+}
+
+function UpdateTodo({item, id}) {
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const [todo, setTodo] = useState(item)
+    const {fetchTodos} = React.useContext(TodosContext)
+
+    const updateTodo = async () => {
+        await fetch(`http://localhost:8000/todo/${id}`, {
+            method:"PUT",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify( {item: todo} )
+        })
+        onClose()
+        await fetchTodos()
+    }
+
+    return (
+        <>
+            <Button h="1.5rem" size="sm" onClick={onOpen}>Update Todo</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader> Update Todo </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <InputGroup size="md">
+                            <Input
+                            pr="4.5rem"
+                            type="text"
+                            placeholder="Add a todo item"
+                            aria-label="Add a todo item"
+                            value={todo}
+                            onChange={e => setTodo(e.target.value)}
+                            />
+                        </InputGroup>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button h="1.5rem" size="sm" onClick={updateTodo}>Update Todo</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
+    )
 }
